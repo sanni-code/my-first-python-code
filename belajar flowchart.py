256e96dbc2e3ffe78882513bc8724ff6f9ed4f97
fitriaulias <fitriauliasanni@gmail.com>
2021-10-15 15:45:02 +0700

    membuat sekuensial

---
+++ belajar flowchart.py
@@ -0,0 +1,22 @@
+"""
+Semua sintaksis dasar bahasa pemrograman terdiri dari:
+1. Sekuensial: langkah berurutan
+2. Percabangan: langkah melompat jika kondisi terpenuhi
+3. Perulangan: mengulang langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
+
+Setiap flowchart dimulai dengan start, kalo udah expert bisa skip langkah ini loh.
+Terminator: Menentukan langkah awal dan akhir suatu program, bentuknya kaya kue kong guan rasa coklat
+Processing: Memproses (rectangle, atau persegi panjang)
+
+Print adalah perintah menulis teks
+Untuk mengetahui kapan menggunakan petik satu dan petik dua maka cek dulu dalamnya harus menggunakan tanda yang beda.
+String = teks
+"""
+
+# sekuensial
+
+print('Ibu berkata. "Pergi ke Toko"')
+print('Budi menjawab, "Baik, apa yang harus saya lakukan di toko?"')
+print('Ibu menjawab, "Beikan susu 1 botol, jika ada telur, beli 6"')
+print("Maka Budi berangkat ke Toko")
+print("Dan mulai berbelanja")
